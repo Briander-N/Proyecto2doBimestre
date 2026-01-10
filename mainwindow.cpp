29fdc38156963c8123eff5fbdcbcda3f27352ad7
alejandro4418 <alejandro.aguirre@epn.edu.ec>
2026-01-09 21:12:46 -0500

    Estructura principal y primera funcion
    Funcion-Registrar juego

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+/*#include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -11,4 +11,44 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+struct Juego{
+	string nombre;
+	string categoria;
+	string desarrollador;
+	float precio;
+	int anioPublicacion;
+};
+
+void registrarJuego(vector<Juego>& juegos){
+	Juego nuevo;
+	cout<<"\nNombre: ";
+	getline(cin, nuevo.nombre);
+	cout<<"Categoria: ";
+	getline(cin, nuevo.categoria);
+	cout<<"Desarrollador: ";
+	getline(cin, nuevo.desarrollador);
+	cout<<"Precio: ";
+	cin>>nuevo.precio;
+	cout<<"Anio de publicacion: ";
+	cin>>nuevo.anioPublicacion;
+	cin.ignore();
+	juegos.push_back(nuevo);
+	cout<<"Juego registrado exitosamente\n";
+}
+
+int main(){
+	vector<Juego> juegos;
+	cout<<"===Gestion de Videojuegos de Steam===\n";
+	cout<<"---------------------------------\n";
+	cout<<"\n---Registrar Juego---\n";
+	registrarJuego(juegos);
+	
+	return 0;
 }
