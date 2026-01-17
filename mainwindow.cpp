bcea92e5c4f61bf13c6ec8db2b25bd35e6aad149
Jordy1199 <69232699+Jordy1199@users.noreply.github.com>
2026-01-16 21:49:35 -0500

    Se agrega la función modificar y validaciones

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,8 @@ MainWindow::~MainWindow()
 #include<vector>
 #include<string>
 #include<fstream>
+#include <locale>
+#include <windows.h>
 using namespace std;
 
 struct Juego{
@@ -92,13 +94,16 @@ vector<Juego> cargarJuegos(){
             }
         }
     }
-    return juegos;	
+    return juegos;
 }
 
 
 void registrarJuego(){
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	setlocale(LC_ALL, "");
 	Juego nuevo;
-	cout<<"Ingresa los datos de registro a continuacion"<<endl;
+	cout<<"Ingresa los datos de registro a continuación"<<endl;
 	cout<<"ID: ";
 	cin>>nuevo.id;
 	cin.ignore();
@@ -106,38 +111,41 @@ void registrarJuego(){
 		cout<<"\nNombre: ";
 		getline(cin, nuevo.nombre);
 		if(nuevo.nombre.empty()){
-			cout<<"El nombre no puede estar vacio!!"<<endl;
+			cout<<"El nombre no puede estar vacío!!"<<endl;
 		}
 	}while(nuevo.nombre.empty());
 
-	cout<<"Categoria: ";
+	cout<<"Categoría: ";
 	getline(cin, nuevo.categoria);
 	cout<<"Desarrollador: ";
 	getline(cin, nuevo.desarrollador);
-	
+
 	do{
 		cout<<"Precio: $";
 		cin>>nuevo.precio;
 		if(nuevo.precio < 0){
-			cout<<"Ingrese un valor valido!"<<endl;
+			cout<<"Ingrese un valor válido!"<<endl;
 		}
 	}while(nuevo.precio < 0);
-	
+
 	do{
-		cout<<"Anio de publicacion: ";
+		cout<<"Anio de publicación: ";
 		cin>>nuevo.anioPublicacion;
 		if(nuevo.anioPublicacion < 1950 || nuevo.anioPublicacion > 2026){
-		    cout << "Ingresa un anio valido (1950 - 2026)"<<endl;
+		    cout << "Ingresa un anio válido (1950 - 2026)"<<endl;
 		}
 	}while(nuevo.anioPublicacion < 1950 || nuevo.anioPublicacion > 2026);
-	
+
 	cin.ignore();
 	guardarJuego(nuevo);
-	
+
 	cout<<"Juego registrado exitosamente\n";
 }
 
 void mostrarJuegos(){
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	setlocale(LC_ALL, "");
     vector<Juego> juegos = cargarJuegos();
 	if(juegos.empty()){
 		cout << "\nNo hay juegos registrados.\n";
@@ -148,29 +156,108 @@ void mostrarJuegos(){
 	for(int i = 0; i < juegos.size(); i++){
 		cout << "ID: " << juegos[i].id << endl;
 		cout << "Nombre: " << juegos[i].nombre << endl;
-		cout << "Categoria: " << juegos[i].categoria << endl;
+		cout << "Categoría: " << juegos[i].categoria << endl;
 		cout << "Desarrollador: " << juegos[i].desarrollador << endl;
 		cout << "Precio: $" << juegos[i].precio << endl;
-		cout << "Anio de publicacion: " << juegos[i].anioPublicacion << endl;
+		cout << "Año de publicación: " << juegos[i].anioPublicacion << endl;
 		cout << "----------------------\n";
 	}
 }
 
+void modificarJuego(){
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	setlocale(LC_ALL, "");
+    vector<Juego> juegos = cargarJuegos();
+
+    if(juegos.empty()){
+        cout << "\nNo hay juegos registrados.\n";
+        return;
+    }
+
+    int idBuscado;
+    cout << "\nIngrese el ID del juego a modificar: ";
+    cin >> idBuscado;
+    cin.ignore();
+
+    bool encontrado = false;
+
+    for(int i = 0; i < juegos.size(); i++){
+        if(juegos[i].id == idBuscado){
+            encontrado = true;
+
+            cout << "\nModificando juego\n";
+
+            do{
+                cout << "Nuevo nombre: ";
+                getline(cin, juegos[i].nombre);
+                if(juegos[i].nombre.empty()){
+                    cout << "El nombre no puede estar vacío.\n";
+                }
+            }while(juegos[i].nombre.empty());
+
+            do{
+                cout << "Nueva categoría: ";
+                getline(cin, juegos[i].categoria);
+                if(juegos[i].categoria.empty()){
+                    cout << "La categoría no puede estar vacía.\n";
+                }
+            }while(juegos[i].categoria.empty());
+
+            do{
+                cout << "Nuevo desarrollador: ";
+                getline(cin, juegos[i].desarrollador);
+                if(juegos[i].desarrollador.empty()){
+                    cout << "El desarrollador no puede estar vacío.\n";
+                }
+            }while(juegos[i].desarrollador.empty());
+
+            do{
+                cout << "Nuevo precio: $";
+                cin >> juegos[i].precio;
+                if(juegos[i].precio < 0){
+                    cout << "Ingrese un precio válido.\n";
+                }
+            }while(juegos[i].precio < 0);
+
+            do{
+                cout << "Nuevo año de publicación: ";
+                cin >> juegos[i].anioPublicacion;
+                if(juegos[i].anioPublicacion < 1950 || juegos[i].anioPublicacion > 2026){
+                    cout << "Ingrese un año válido (1950 - 2026).\n";
+                }
+            }while(juegos[i].anioPublicacion < 1950 || juegos[i].anioPublicacion > 2026);
+
+            guardarTodosJuegos(juegos);
+            cout << "\nJuego modificado correctamente.\n";
+            break;
+        }
+    }
+
+    if(!encontrado){
+        cout << "\nNo se encontró un juego con ese ID.\n";
+    }
+}
+
+
 void eliminarJuego(){
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	setlocale(LC_ALL, "");
 	vector<Juego> juegos = cargarJuegos();
 	if(juegos.empty()){
 		cout<<"\nNo hay juegos registrados.\n";
 		return;
 	}
 	cout<<"\n--- Eliminar Juego ---\n";
-	
+
 	int idBuscado;
 	cout<<"Ingrese el id del juego a eliminar: ";
 	cin>>idBuscado;
 	bool encontrado = false;
 	for (int i=0; i<juegos.size();i++){
 		if (juegos[i].id == idBuscado){
-			cout<<"Eliminando el juego : "<<juegos[i].nombre<<endl; 
+			cout<<"Eliminando el juego : "<<juegos[i].nombre<<endl;
 			encontrado = true;
 			juegos.erase(juegos.begin() + i);
 			guardarTodosJuegos(juegos);
@@ -179,21 +266,24 @@ void eliminarJuego(){
 		}
 	}
 	if (!encontrado){
-		cout<<"No se encontro un juego con la ID ingresada"<<endl;
+		cout<<"No se encontró un juego con la ID ingresada"<<endl;
 	}
 }
 
 int main(){
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	setlocale(LC_ALL, "");
 	vector<Juego> juegos;
 	int opcion;
 	do{
-		cout<<"===Gestion de Videojuegos de Steam===\n";
+		cout<<"===Gestión de Videojuegos de Steam===\n";
 		cout<<"1. Registrar Juego"<<endl;
 		cout<<"2. Ver juegos"<<endl;
 		cout<<"3. Modificar juego"<<endl;
 		cout<<"4. Eliminar juego"<<endl;
 		cout<<"0. Salir"<<endl;
-		
+
 		cout<<"Ingresa una opcion: ";
 		cin>>opcion;
 		switch (opcion){
@@ -204,7 +294,7 @@ int main(){
 				mostrarJuegos();
 				break;
 			case 3:
-				
+				modificarJuego();
 				break;
 			case 4:
 				eliminarJuego();
@@ -213,10 +303,9 @@ int main(){
 				cout<<"Saliendo..."<<endl;
 				break;
 			default:
-				cout<<"Opcion no valida";
+				cout<<"Opción no valida";
 				break;
 		}
     }while (opcion != 0);
     return 0;
 }
-
