0c68d23e4604ac22c3fc8001dfdab20352df6707
alejandro4418 <alejandro.aguirre@epn.edu.ec>
2026-01-20 20:03:09 -0500

    Struct Juego

---
+++ Juego.h
@@ -0,0 +1,15 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include <QString>
+
+struct Juego{
+    int id;
+    QString nombre;
+    QString categoria;
+    QString desarrollador;
+    float precio;
+    int anioPublicacion;
+};
+
+#endif // JUEGO_H
